c57679a32963cc078ca9f910d70c18b6153bf120
山田 卓史 <yamadatakashi@yamada-no-MacBook-Air-2.local>
2012-12-13 14:03:46 +0900

    firstcomiit

---
+++ HelloGitHub2/HelloGitHub2ViewSecondController.h
@@ -0,0 +1,13 @@
+//
+//  HelloGitHub2ViewSecondController.h
+//  HelloGitHub2
+//
+//  Created by 山田 卓史 on 12/12/13.
+//  Copyright (c) 2012年 SWEET ROOM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HelloGitHub2ViewSecondController : UIViewController
+
+@end
---
+++ HelloGitHub2/HelloGitHub2ViewSecondController.m
@@ -0,0 +1,38 @@
+//
+//  HelloGitHub2ViewSecondController.m
+//  HelloGitHub2
+//
+//  Created by 山田 卓史 on 12/12/13.
+//  Copyright (c) 2012年 SWEET ROOM. All rights reserved.
+//
+
+#import "HelloGitHub2ViewSecondController.h"
+
+@interface HelloGitHub2ViewSecondController ()
+
+@end
+
+@implementation HelloGitHub2ViewSecondController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
