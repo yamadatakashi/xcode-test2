1667ee61b5966556a22716b2205733ea64a0ff51
山田 卓史 <yamadatakashi@yamada-no-MacBook-Air-2.local>
2012-12-13 14:05:05 +0900

    second

---
+++ HelloGitHub2ThridViewController.m
@@ -0,0 +1,38 @@
+//
+//  HelloGitHub2ThridViewController.m
+//  HelloGitHub2
+//
+//  Created by 山田 卓史 on 12/12/13.
+//  Copyright (c) 2012年 SWEET ROOM. All rights reserved.
+//
+
+#import "HelloGitHub2ThridViewController.h"
+
+@interface HelloGitHub2ThridViewController ()
+
+@end
+
+@implementation HelloGitHub2ThridViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
